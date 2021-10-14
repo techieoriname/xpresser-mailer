3ec6c09ac226036bcb0dbe9144e594e2a967e181
Techie Oriname <33547272+techieoriname@users.noreply.github.com>
2021-10-14 16:01:49 +0100

    first commit

---
+++ index.ts
@@ -0,0 +1,58 @@
+import nodemailer, { SendMailOptions } from "nodemailer";
+import { getInstance } from "xpresser";
+import aws, { SES } from "@aws-sdk/client-ses";
+import { Address, AttachmentLike } from "nodemailer/lib/mailer";
+import { Readable } from "stream";
+
+const $ = getInstance();
+
+// Get config
+const config = $.config.path("mailer");
+
+let transporter: any;
+
+if (config.get("provider") === "AWS") {
+    process.env.AWS_ACCESS_KEY_ID = config.get("AWS_ACCESS_KEY_ID");
+    process.env.AWS_SECRET_ACCESS_KEY = config.get("AWS_SECRET_ACCESS_KEY");
+
+    // Set the AWS Region.
+    const REGION = config.get("region"); //e.g. "us-east-1"
+    // const sesClient = new SESClient({ region: REGION });
+    const ses = new SES({
+        apiVersion: "2010-12-01", // lock api version
+        region: REGION
+    });
+
+    transporter = nodemailer.createTransport({
+        SES: { ses, aws },
+        sendingRate: config.get("sendingRate") || 1
+    });
+} else {
+    transporter = nodemailer.createTransport({
+        host: config.get("host"),
+        port: config.get("port"),
+        ...(config.get("port") === 465 ? { secure: true } : { secure: false }),
+        auth: {
+            user: config.get("username"),
+            pass: config.get("password")
+        }
+    });
+}
+
+export const sendMail = async (
+    $to: string | Address | Array<string | Address>,
+    $subject: string,
+    $message: string | Buffer | Readable | AttachmentLike,
+    $messageType: string = "html"
+): Promise<void> => {
+    const from = config.get("from") || $.config.get("name");
+
+    const mail: SendMailOptions = {
+        from: `${from} <${config.get("fromEmail")}>`,
+        to: $to,
+        subject: $subject,
+        ...($messageType === "html" ? { html: $message } : { text: $message })
+    };
+
+    await transporter.sendMail(mail);
+};
---
+++ plugin-index.ts
@@ -0,0 +1,46 @@
+import { DollarSign } from "xpresser/types";
+
+const packageName = "mailer";
+
+export function run(plugin: any, $: DollarSign) {
+    $.ifNotConsole(() => {
+        if (!$.config.has(packageName)) {
+            return $.logErrorAndExit(`Config "maiiler" is required!`);
+        }
+        const config = $.config.path(packageName);
+
+        if (!config.has("provider")) {
+            return $.logErrorAndExit(`"provider" is required in config: "${packageName}"`);
+        }
+
+        if (!config.get("provider").match(/^(AWS|SMTP)$/)) {
+            return $.logErrorAndExit(
+                `"${config.get("provider")}" is not a valid provider {SMTP | AWS}`
+            );
+        }
+
+        if (config.get("provider") === "AWS") {
+            let requiredFields = [
+                "region",
+                "fromEmail",
+                "AWS_SECRET_ACCESS_KEY",
+                "AWS_ACCESS_KEY_ID"
+            ];
+            requiredFields.forEach((value) => {
+                if (!config.get(value)) {
+                    return $.logErrorAndExit(`mailer {${value}} is missing!`);
+                }
+            });
+        } else {
+            let requiredFields = ["host", "port", "username", "password", "fromEmail"];
+
+            requiredFields.forEach((value) => {
+                if (!config.get(value)) {
+                    return $.logErrorAndExit(`mailer {${value}} is missing!`);
+                }
+            });
+        }
+
+        require("./index");
+    });
+}
