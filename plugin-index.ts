b67469f628a2d264a056695cd78c50b3fea4e265
trapcodeio <trapcode.io@gmail.com>
2021-11-05 21:15:46 +0100

    A few refactors

--- plugin-index.ts
+++ plugin-index.ts
@@ -1,37 +1,47 @@
 import { DollarSign } from "xpresser/types";
 
-const packageName = "mailer";
 
 export function run(plugin: any, $: DollarSign) {
+    const packageName = plugin.namespace;
+
     $.ifNotConsole(() => {
+        // Check package config
         if (!$.config.has(packageName)) {
-            return $.logErrorAndExit(`Config "maiiler" is required!`);
+            return $.logErrorAndExit(`Config ${packageName}" is required!`);
         }
+
+        // Validate package config.
         const config = $.config.path(packageName);
 
-        if (!config.has("provider")) {
+        // Get provider
+        const provider = config.get("provider") as string | undefined;
+        if (!provider) {
             return $.logErrorAndExit(`"provider" is required in config: "${packageName}"`);
         }
 
-        if (!config.get("provider").match(/^(AWS|SMTP)$/)) {
+        // validate provider.
+        if (!provider.match(/^(AWS|SMTP)$/)) {
             return $.logErrorAndExit(
                 `"${config.get("provider")}" is not a valid provider {SMTP | AWS}`
             );
         }
 
-        if (config.get("provider") === "AWS") {
+        if (provider === "AWS") {
             let requiredFields = [
                 "region",
                 "fromEmail",
                 "AWS_SECRET_ACCESS_KEY",
                 "AWS_ACCESS_KEY_ID"
             ];
+
             requiredFields.forEach((value) => {
                 if (!config.get(value)) {
                     return $.logErrorAndExit(`mailer {${value}} is missing!`);
                 }
             });
+
         } else {
+
             let requiredFields = ["host", "port", "username", "password", "fromEmail"];
 
             requiredFields.forEach((value) => {
